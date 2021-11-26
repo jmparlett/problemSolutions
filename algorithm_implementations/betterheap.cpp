4274d901284cccb930925aa16c7b1858a4e17194
jmparlett <48170846+jmparlett@users.noreply.github.com>
2021-11-26 21:42:35 +0000

    Update betterheap.cpp

--- algorithm_implementations/betterheap.cpp
+++ algorithm_implementations/betterheap.cpp
@@ -62,7 +62,7 @@ void upheap(heap* H, int pos){
   //if you are root stop
   if(pos-1 < 0) return;
 
-  //if your less than you parent stop
+  //if your greater than you parent stop
   if(H->arr[parent(pos)] <= H->arr[pos]) return;
 
   //else swap with parent and recurse
